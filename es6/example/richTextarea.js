29a40c0bc4b8459185038cca2a2fd7c8aa167dc9
James Smith <james.smith@djalbat.com>
2020-05-02 17:43:36 +0100

    A tiny, cosmetic change to the example.

--- es6/example/richTextarea.js
+++ es6/example/richTextarea.js
@@ -6,7 +6,7 @@ import { RichTextarea } from "../index";
 
 export default withStyle(RichTextarea)`
 
-  border: 1px solid darkgray;
+  border: 1px solid darkgrey;
   width: 32rem;
   height: 32rem;
   padding: 0.25rem;
